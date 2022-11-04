e01a0f6b24876a06c36f51ad0c65c2db01056c74
Rae Hebron <rae.oxford.hebron@gmail.com>
2022-11-04 17:47:46 -0400

    finished last part of take one down, pass it around

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ app.get('/', (request, response) => {
     <a href='http://localhost:3000/98'>Take one down, pass it around</a>`)
 })
 
+app.get('/0', (request, response) => {
+    response.send(`<a href='http://localhost:3000/'>Start Over</a>`)
+})
+
 app.get('/:number_of_bottles', (request, response) => {
     response.send(`${request.params.number_of_bottles} Bottles of beer on the wall
     <a href='http://localhost:3000/${request.params.number_of_bottles -1}'>Take one down, pass it around</a>`)
 })
-
 
 app.listen(PORT, () => {
     console.log(`listening to ${PORT}`)
