97ed51eb7aeafe15dd5e453c46b2c74f1f13834b
Rae Hebron <rae.oxford.hebron@gmail.com>
2022-11-04 17:43:28 -0400

    created express application for take one down and pass it around

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+app.get('/', (request, response) => {
+    response.send(`99 Bottles of beer on the wall
+    <a href='http://localhost:3000/98'>Take one down, pass it around</a>`)
+})
+
+app.get('/:number_of_bottles', (request, response) => {
+    response.send(`${request.params.number_of_bottles} Bottles of beer on the wall
+    <a href='http://localhost:3000/${request.params.number_of_bottles -1}'>Take one down, pass it around</a>`)
+})
+
+
+app.listen(PORT, () => {
+    console.log(`listening to ${PORT}`)
+})
