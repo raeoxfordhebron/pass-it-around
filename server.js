614ede566dfe6d790d086ed34205b6563ffc9c78
Rae Hebron <rae.oxford.hebron@gmail.com>
2022-11-05 10:39:29 -0400

    created random number aspect

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = 3000
 
 app.get('/', (request, response) => {
-    response.send(`99 Bottles of beer on the wall
+    response.send(`99 little bugs in the code, 99 little bugs. 
     <a href='http://localhost:3000/98'>Take one down, pass it around</a>`)
 })
 
@@ -12,8 +12,9 @@ app.get('/0', (request, response) => {
 })
 
 app.get('/:number_of_bottles', (request, response) => {
-    response.send(`${request.params.number_of_bottles} Bottles of beer on the wall
-    <a href='http://localhost:3000/${request.params.number_of_bottles -1}'>Take one down, pass it around</a>`)
+    response.send(`${request.params.number_of_bottles} little bugs in the code, ${request.params.number_of_bottles} little bugs
+    <a href='http://localhost:3000/${request.params.number_of_bottles -1}'>Take one down, pass it around</a>
+    ${Math.floor(Math.random()*100)} bugs in the code`)
 })
 
 app.listen(PORT, () => {
